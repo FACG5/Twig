3f40b43f41dd8c3dcf2549844c0aff3f09947456
hemasali <hemo3li7@gmail.com>
2018-11-06 14:49:56 +0200

    x

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+library.add(faTimesCircle)
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
