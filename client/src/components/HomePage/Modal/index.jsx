844a64f19e6a593a62edf5ee52f4d46ac0fe4e89
hemasali <hemo3li7@gmail.com>
2018-11-07 10:26:13 +0200

    fixing eslint problems

--- client/src/components/HomePage/Modal/index.jsx
+++ client/src/components/HomePage/Modal/index.jsx
@@ -7,25 +7,24 @@ export default class Modal extends Component {
   state = {
     login: true,
   }
+
   switch = () => {
-    this.setState({ login: !this.state.login });    
+    this.setState(prevState => (
+      { show: !prevState.show }
+    ));
   };
-  onChange = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({ [name]: value });
-  };
+
   render() {
     const { show } = this.props;
     const { login } = this.state;
     return (
-      <div className='modal'>
-        {login? <Login show={show} switchModel = {this.switch}/> : null }
+      <div className="modal">
+        {login ? <Login show={show} switchModel={this.switch} /> : null }
       </div>
     );
   }
 }
 
 Modal.propTypes = {
-  show: PropTypes.func.isRequired
+  show: PropTypes.func.isRequired,
 };
