79f649bff5ff2c459feb71094bdbee7cde220dcf
hemasali <hemo3li7@gmail.com>
2018-11-07 22:15:14 +0200

    Work in progress - create Join Details Model Jsx file

---
+++ client/src/components/HomePage/modal/JoinModal/JoinDetails/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.css';
+import PropTypes from 'prop-types';
+import Button from '../../../../common/Button';
+import Inputs from '../../../../common/Inputs';
+
+class JoinDetails extends Component {
+  state = {};
+
+  render() {
+    const { onChange, logState } = this.props;
+    return (
+     
+    );
+  }
+}
+
+export default JoinDetails;
