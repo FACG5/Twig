69d0065a5728bd4eb4921492ca23daffb2c3150d
lubnaabad <lubna_mosa@hotmail.com>
2018-11-07 08:43:18 +0200

    add header common components relates #28

---
+++ client/src/components/common/Header/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Button from '../Button/index';
+import './style.css';
+import '../Button/style.css';
+import Logo from './logo1.png';
+
+class Header extends Component {
+  state= {};
+  render() {
+    return (
+      <div className="header">
+        <img src={Logo} alt="logo" className="header__logo" />
+        <div className="header__buttons">
+          <Button value="join" className="join" />
+          <Button value="login" className="login" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
