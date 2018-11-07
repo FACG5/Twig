5335e153fa6e4ca5b4e462b818c1a5bf77600aeb
hemasali <hemo3li7@gmail.com>
2018-11-07 12:06:30 +0200

    set states of models login and join and create functions of show and hide models

--- client/src/components/common/Header/index.jsx
+++ client/src/components/common/Header/index.jsx
@@ -3,25 +3,72 @@ import Button from '../Button/index';
 import './style.css';
 import '../Button/style.css';
 import Logo from './logo1.png';
+import LoginModel from '../../HomePage/modal/LoginModel';
+import JoinModel from '../../HomePage/modal/JoinModel';
 
 class Header extends Component {
   state = {
-    model: false,
+    joinModel: false,
+    loginModel: false,
   };
 
-  showModel =() => {
-    this.setState(prevState => ({ model: !prevState.model }));
+  showModel = (event) => {
+    const { id } = event.target;
+    this.setState(
+      {
+        joinModel: false,
+        loginModel: false,
+      },
+      () => {
+        this.setState({ [id]: true });
+      },
+    );
+  };
+
+  closeModel = () => {
+    this.setState({ joinModel: false, loginModel: false });
+  };
+
+  switchModel = () => {
+    this.setState(prevState => (
+      { joinModel: !prevState.joinModel, loginModel: !prevState.loginModel }
+    ));
   }
 
   render() {
+    const { joinModel, loginModel } = this.state;
     return (
-      <div className="header">
-        <img src={Logo} alt="logo" className="header__logo" />
-        <div className="header__buttons">
-          <Button value="join" className="join" onClick={() => this.showModel} join />
-          <Button value="login" className="login" onClick={() => this.showModel} login />
+      <React.Fragment>
+        <div className="header">
+          <img src={Logo} alt="logo" className="header__logo" />
+          <div className="header__buttons">
+            <Button
+              value="join"
+              className="join"
+              onClick={this.showModel}
+              id="joinModel"
+            />
+            <Button
+              value="login"
+              className="login"
+              onClick={this.showModel}
+              id="loginModel"
+            />
+          </div>
         </div>
-      </div>
+        {loginModel ? (
+          <LoginModel
+            closeModel={this.closeModel}
+            switchModel={this.switchModel}
+          />
+        ) : null}
+        {joinModel ? (
+          <JoinModel
+            closeModel={this.closeModel}
+            switchModel={this.switchModel}
+          />
+        ) : null}
+      </React.Fragment>
     );
   }
 }
