f9828af63c45d7c1c5f6be9093aff86ee40040cd
lubnaabad <lubna_mosa@hotmail.com>
2018-11-07 14:12:30 +0200

    create file aboutUs in component folder

---
+++ client/src/components/HomePage/AboutUs/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1 className="about__text">About Us</h1>
+    </div>
+  );
+}
