193fedb157f71de1b2aaa3a0ef59fd9108d1e025
hemasali <hemo3li7@gmail.com>
2018-11-08 22:31:14 +0200

    sent Set job title to Stage two so i can send it to main Join model

--- client/src/components/HomePage/modal/JoinModal/index.jsx
+++ client/src/components/HomePage/modal/JoinModal/index.jsx
@@ -5,36 +5,57 @@ import PropTypes from 'prop-types';
 import Input from '../../../common/Inputs';
 import Button from '../../../common/Button';
 import JoinDetails from './JoinDetails';
+import PopUp from './ValidationPopUp';
 
 class Join extends Component {
-  state = { completeJoin: false };
+  state = {
+    completeJoin: false,
+    validPopUp: false,
+  };
 
   onChange = (event) => {
+    this.storeValue(event);
+  };
+
+  storeValue = (event) => {
     const { target } = event;
-    const { value, parentNode } = target;
+    const { value } = target;
     let valutToSave = '';
     if (target.type === 'checkbox') {
       valutToSave = target.checked;
-    } else if (target.type === 'radio') {
-      const { innerText } = parentNode;
-      valutToSave = innerText;
     } else {
       valutToSave = value;
     }
-
     const { name } = target;
     this.setState({
       [name]: valutToSave,
     });
   };
 
-  completeJoinCheck = () => {
+  setJobTitle = (value) => {
+    this.setState({ jobTitle: value });
+  }
+
+  validation = (event) => {
+    const { target } = event;
+    const { value } = target;
+    if (!value.trim()) return event.target.classList.add('input__success');
+    return event.target.classList.remove('input__success');
+  };
+
+  joinCheck = () => {
     const {
       first, last, email, password,
     } = this.state;
     if (first && last && email && password) {
       this.setState({ completeJoin: true });
+    } else {
+      this.setState({ validPopUp: true });
     }
+  };
+
+  closePopUp = () => {
+    this.setState({ validPopUp: false });
   };
 
   backFromDetails = () => {
@@ -43,78 +64,97 @@ class Join extends Component {
 
   render() {
     const { closeModel, switchModel } = this.props;
-    const { completeJoin } = this.state;
+    const {
+      completeJoin,
+      validPopUp,
+      first,
+      last,
+      email,
+      password,
+    } = this.state;
     return (
-      <div className="modal">
-        {!completeJoin ? (
-          <div className="modelContent">
-            <div className="modalHead">
-              <h1>Join TWIG</h1>
-              <FontAwesomeIcon
-                icon="times-circle"
-                className="closeButton"
-                onClick={closeModel}
-              />
-            </div>
-            <hr />
-            <div className="content">
-              <div>
-                <Input
-                  name="first"
-                  type="text"
-                  className="join__name"
-                  placeholder="First Name"
-                  onChange={this.onChange}
-                />
-                <Input
-                  name="last"
-                  type="text"
-                  className="join__name"
-                  placeholder="Last Name"
-                  onChange={this.onChange}
+      <React.Fragment>
+        <div className="modal">
+          {!completeJoin ? (
+            <div className="modelContent">
+              <div className="modalHead">
+                <h1>Join TWIG</h1>
+                <FontAwesomeIcon
+                  icon="times-circle"
+                  className="closeButton"
+                  onClick={closeModel}
                 />
               </div>
-              <Input
-                name="email"
-                type="email"
-                className="input__email"
-                placeholder="Enter your email"
-                onChange={this.onChange}
-              />
-              <Input
-                name="password"
-                type="password"
-                className="input__password"
-                placeholder="Enter your password"
-                onChange={this.onChange}
-              />
+              <hr />
+              <div className="content">
+                <div>
+                  <Input
+                    name="first"
+                    type="text"
+                    className="join__name"
+                    placeholder="First Name"
+                    onChange={this.onChange}
+                    onBlur={this.validation}
+                    value={first}
+                  />
+                  <Input
+                    name="last"
+                    type="text"
+                    className="join__name"
+                    placeholder="Last Name"
+                    onChange={this.onChange}
+                    onBlur={this.validation}
+                    value={last}
+                  />
+                </div>
+                <Input
+                  name="email"
+                  type="email"
+                  className="input__email"
+                  placeholder="Enter your email"
+                  onChange={this.onChange}
+                  onBlur={this.validation}
+                  value={email}
+                />
+                <Input
+                  name="password"
+                  type="password"
+                  className="input__password"
+                  placeholder="Enter your password"
+                  onChange={this.onChange}
+                  onBlur={this.validation}
+                  value={password}
+                />
+                <Button
+                  className="button__join"
+                  value="Join"
+                  onClick={() => this.joinCheck()}
+                  id="completeJoin"
+                />
+                <h3>Or</h3>
+                <Button
+                  className="button__linkedin"
+                  value="Join with LinkedIn"
+                  onClick={null}
+                />
+              </div>
+              <hr />
               <Button
-                className="button__join"
-                value="Join"
-                onClick={() => this.completeJoinCheck()}
-                id="completeJoin"
-              />
-              <h3>Or</h3>
-              <Button
-                className="button__linkedin"
-                value="Join with LinkedIn"
-                onClick={null}
+                className="join__backtologin"
+                value="Back To Login"
+                onClick={switchModel}
               />
             </div>
-            <hr />
-            <Button
-              className="join__backtologin"
-              value="Back To Login"
-              onClick={switchModel}
+          ) : (
+            <JoinDetails
+              onChange={this.onChange}
+              backFromDetails={this.backFromDetails}
+              setJobTitle={this.setJobTitle}
             />
-          </div>
-        ) : (
-          <JoinDetails
-            onChange={this.onChange}
-            backFromDetails={this.backFromDetails}
-          />
-        )}
-      </div>
+          )}
+        </div>
+        {validPopUp ? <PopUp closePopUp={this.closePopUp} /> : null}
+      </React.Fragment>
     );
   }
 }
