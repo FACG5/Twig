b7840838c3350fdbb91c1c7430fd09de93a80642
lubnaabad <lubna_mosa@hotmail.com>
2018-11-08 09:45:21 +0200

    add statistic components

--- client/src/components/HomePage/index.jsx
+++ client/src/components/HomePage/index.jsx
@@ -1,16 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from '../common/Header/index';
 import AboutUs from './AboutUs/index';
+import Statistic from './Statistic/index';
 import './style.css';
 
 export default function HomePage() {
   return (
-    <Fragment>
+    <div>
       <Header />
       <div className="home__image">
         <h1 className="home__text">Medical Translation</h1>
       </div>
-      <div className="home__section"><AboutUs /></div>
-    </Fragment>
+      <div className="home__main">
+        <div className="home__section">
+          <div className="contents">
+            <AboutUs />
+            <Statistic />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
