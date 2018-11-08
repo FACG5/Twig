961d519c89e2836b6efdde8f8d133832999a3347
hemasali <hemo3li7@gmail.com>
2018-11-08 11:40:27 +0200

    call the function in back button to back to previous stage

--- client/src/components/HomePage/modal/JoinModal/JoinDetails/StageOne/index.jsx
+++ client/src/components/HomePage/modal/JoinModal/JoinDetails/StageOne/index.jsx
@@ -1,48 +1,83 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../../../../common/Button';
 import Inputs from '../../../../../common/Inputs';
 import './style.css';
 
-
 export default function StageOne(props) {
-  const { onChange, logState, changeStage } = props;
+  const {
+    onChange, logState, changeStage, backFromDetails,
+  } = props;
   return (
-    <Fragment>
+    <div className="contentOfDetails">
       <h4 className="completeTitle"> Tell us About Your skills in English </h4>
       <hr />
-        <div className="Boxes">
-          <label className="containerCheckBox">
+      <div className="Boxes">
+        <label className="containerCheckBox">
           I’m a native speaker / mothertongue.
-            <Inputs className="" placeholder="" onChange={onChange} type="checkbox" checked="checked" name="native" />
-            <span className="checkBox" />
-          </label>
-          <label className="containerCheckBox">
+          <Inputs
+            className=""
+            placeholder=""
+            id="native"
+            onChange={onChange}
+            type="checkbox"
+            checked="checked"
+            name="native"
+          />
+          <span className="checkBox" />
+        </label>
+        <label className="containerCheckBox">
           I have tested at upper intermediate or advanced level.
-            <Inputs className="" placeholder="" onChange={onChange} type="checkbox" name="intemediate" />
-            <span className="checkBox" />
-          </label>
-          <label className="containerCheckBox">
+          <Inputs
+            className=""
+            placeholder=""
+            onChange={onChange}
+            type="checkbox"
+            name="intemediate"
+            id="intemediate"
+          />
+          <span className="checkBox" />
+        </label>
+        <label className="containerCheckBox">
           I have completed University or professional training this language.
-            <Inputs className="" placeholder="" onChange={onChange} type="checkbox" name="university" />
-            <span className="checkBox" />
-          </label>
-          <label className="containerCheckBox">
+          <Inputs
+            className=""
+            placeholder=""
+            onChange={onChange}
+            type="checkbox"
+            name="university"
+          />
+          <span className="checkBox" />
+        </label>
+        <label className="containerCheckBox">
           I’m self-taught.
-            <Inputs className="" placeholder="" onChange={onChange} type="checkbox" name="self" />
-            <span className="checkBox" />
-          </label>
-        </div>
+          <Inputs
+            className=""
+            placeholder=""
+            onChange={onChange}
+            type="checkbox"
+            name="self"
+          />
+          <span className="checkBox" />
+        </label>
+      </div>
       <Button
         className="nextButton"
         id="nextDetails"
         onClick={changeStage}
         value="Next"
       />
-    </Fragment>
+      <Button
+        className="backButton"
+        id="nextDetails"
+        onClick={backFromDetails}
+        value="Back"
+      />
+    </div>
   );
 }
 StageOne.propTypes = {
   onChange: PropTypes.func.isRequired,
   changeStage: PropTypes.func.isRequired,
+  backFromDetails: PropTypes.func.isRequired,
 };
