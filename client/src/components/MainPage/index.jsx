c6a32bc0dfbe6748d5c3f28ffcbbacdefe1cda05
ali <ali.7@outlook.com>
2018-11-08 11:28:05 +0200

    create-mainpage-base-component relate  #17

---
+++ client/src/components/MainPage/index.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from '../common/Header/index';
+import Footer from '../common/Footer/index';
+import './style.css';
+
+export default function MainPage() {
+  return (
+    <Fragment>
+      <Header />
+      <Footer />
+    </Fragment>
+  );
+}
