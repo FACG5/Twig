1b3710f51f8c2f090ad65464cb06bd0b8ca98701
ali <ali.7@outlook.com>
2018-11-08 14:35:45 +0200

    add-routes-to-App relate #62

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage/index';
+import MainPage from './components/MainPage/index';
 
 class App extends Component {
   render() {
     return (
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/categories" component={MainPage} />
+        </Switch>
+      </Router>
     );
   }
 }
