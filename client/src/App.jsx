fc22f7d7fd86b79e5293860f5a2c0ea9888650fc
ali <ali.7@outlook.com>
2018-11-08 14:38:06 +0200

    remove-index-in-imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage/index';
-import MainPage from './components/MainPage/index';
+import HomePage from './components/HomePage';
+import MainPage from './components/MainPage';
 
 class App extends Component {
   render() {
