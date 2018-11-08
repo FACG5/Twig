54d2edd490254e4edbce003127b85c299e957a0a
lubnaabad <lubna_mosa@hotmail.com>
2018-11-08 09:11:22 +0200

    importing fragment in index.js

--- client/src/components/HomePage/index.jsx
+++ client/src/components/HomePage/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from '../common/Header/index';
 import AboutUs from './AboutUs/index';
 import './style.css';
 
 export default function HomePage() {
   return (
-    <React.Fragment>
+    <Fragment>
       <Header />
       <div className="home__image">
         <h1 className="home__text">Medical Translation</h1>
       </div>
       <div className="home__section"><AboutUs /></div>
-    </React.Fragment>
+    </Fragment>
   );
 }
