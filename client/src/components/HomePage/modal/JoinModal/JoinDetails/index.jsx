28f80efa3b44d216d85d05c64339e86b7b8a084a
hemasali <hemo3li7@gmail.com>
2018-11-08 09:38:24 +0200

    jsx file which contain main class of stages of Join details

--- client/src/components/HomePage/modal/JoinModal/JoinDetails/index.jsx
+++ client/src/components/HomePage/modal/JoinModal/JoinDetails/index.jsx
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
-import Button from '../../../../common/Button';
-import Inputs from '../../../../common/Inputs';
+import StageOne from './StageOne/index';
+import StegeTwo from './StageTwo/index';
 
 class JoinDetails extends Component {
-  state = {};
+  state = { firstStage: true };
+
+  changeStage = () => {
+    this.setState(prevStat => ({ firstStage: !prevStat.firstStage }));
+  };
 
   render() {
     const { onChange, logState } = this.props;
+    const { firstStage } = this.state;
     return (
-     
+      <div className="contentOfDetails">
+        {firstStage ? (
+          <StageOne onChange={onChange} logState={logState} changeStage={this.changeStage} />
+        ) : (
+          <StegeTwo onChange={onChange} logState={logState} />
+        )}
+      </div>
     );
   }
 }
+
+JoinDetails.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  logState: PropTypes.func.isRequired,
+};
 
 export default JoinDetails;
