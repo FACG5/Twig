01ae23309b03967821dbefefd42f3410b884b522
lubnaabad <lubna_mosa@hotmail.com>
2018-11-09 16:06:17 +0200

    create progress bar component

---
+++ client/src/components/common/ProgressBar/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class ProgressBar extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="progress">
+        <h2 className="progress__tittel">MY PROFILE</h2>
+        <hr className="progress__line" />
+        <div className="progress__bar">
+          <span className="progress__bar--meter">10% Completed</span>
+        </div>
+        <p className="progress__text">Complete My Profile</p>
+      </div>
+    );
+  }
+}
+
+export default ProgressBar;
