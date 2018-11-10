a4811392b05df0d66ca645cd8b36e87a31a7bc9d
ali <ali.7@outlook.com>
2018-11-10 21:57:56 +0200

    add-ProfileCard-component-to-mainpage relate #26

--- client/src/components/MainPage/index.jsx
+++ client/src/components/MainPage/index.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 import ProgressBar from '../common/ProgressBar';
+import ProfileCard from '../common/ProfileCard';
 import './style.css';
 
 export default function MainPage() {
@@ -9,6 +10,7 @@ export default function MainPage() {
     <Fragment>
       <Header />
       <div className="main">
+        <ProfileCard />
         <ProgressBar />
       </div>
       <Footer />
