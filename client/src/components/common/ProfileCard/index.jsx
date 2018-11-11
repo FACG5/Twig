e87806266b57de736a50239d7e2a4fff699825e3
ali <ali.7@outlook.com>
2018-11-11 11:28:51 +0200

    add-clickHandler-method-to-onClick-profile-button relates #26

--- client/src/components/common/ProfileCard/index.jsx
+++ client/src/components/common/ProfileCard/index.jsx
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import avatarImg from '../../../assets/img_avatar.png';
 import Button from '../Button';
 import './style.css';
 
 class ProfileCard extends Component {
-  state = {};
+  state = {
+    redirect: false,
+  };
+
+  clickHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      redirect: true,
+    });
+  }
 
   render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/profile" />;
+    }
+
     const {
       name, location, language, dialect,
     } = this.props;
     return (
-      <div className="profileCard__main">
+      <div className="profile-card__main">
         <img src={avatarImg} alt="avatarImg" className="avatar__img" />
-        <h1 className="profileCard__name">{ name }</h1>
-        <h1 className="profileCard__location">{ location }</h1>
-        <h1 className="profileCard__lang">
+        <h1 className="profile-card__name">{ name }</h1>
+        <h1 className="profile-card__location">{ location }</h1>
+        <h1 className="profile-card__lang">
           { language }
         -
           { dialect }
         </h1>
-        <Button className="button__profileCard" value="Show Profile" onClick={null} />
+        <Button className="button__profile-card" value="Show Profile" onClick={this.clickHandler} />
       </div>
     );
   }
