1a9a91f9078ae4012f39bdf98228e29a4b654f1c
lubnaabad <lubna_mosa@hotmail.com>
2018-11-11 11:06:06 +0200

    add route  Question page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import MainPage from './components/MainPage';
+import QuestionsPage from './components/QuestionsPage';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/landing" component={HomePage} />
           <Route exact path="/" component={MainPage} />
+          <Route exact path="/specialization" component={QuestionsPage} />
         </Switch>
       </Router>
     );
