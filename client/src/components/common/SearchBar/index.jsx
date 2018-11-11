587a3c8d680349a503a073cc00471929645bea4c
ali <ali.7@outlook.com>
2018-11-11 12:06:10 +0200

    add-onClick-props-to-search-bar-button relates #76

--- client/src/components/common/SearchBar/index.jsx
+++ client/src/components/common/SearchBar/index.jsx
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = (props) => {
-  const { className } = props;
+  const { className, submitHandler } = props;
   return (
     <Fragment>
       <form className={className}>
         <input className="search__text" type="text" placeholder="Search..." name="search" />
-        <button className="search__button" type="submit"><FontAwesomeIcon icon={faSearch} size="lg" /></button>
+        <button className="search__button" type="submit"><FontAwesomeIcon icon={faSearch} size="lg" onClick={submitHandler} /></button>
       </form>
     </Fragment>
   );
@@ -18,6 +18,7 @@ const SearchBar = (props) => {
 
 SearchBar.propTypes = {
   className: PropTypes.string.isRequired,
+  submitHandler: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
