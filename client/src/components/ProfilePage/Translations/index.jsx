1f7cb31ba9ddd247f0c96a3a034bc99620571392
hemasali <hemo3li7@gmail.com>
2018-11-11 18:03:59 +0200

    jsx file of Translation component

---
+++ client/src/components/ProfilePage/Translations/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+export default function Translations() {
+  return (
+    <div className="translations__box">
+      <h3 className="translations__title">Translation</h3>
+      <hr />
+      <div className="translations__content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+          officiis. Aliquid incidunt, perspiciatis eius cum voluptatibus culpa
+          ut corporis temporibus saepe odit tempora sed dolores quo sit fuga
+          deleniti ex?
+        </p>
+      </div>
+    </div>
+  );
+}
