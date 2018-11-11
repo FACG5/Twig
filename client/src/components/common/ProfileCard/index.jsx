16c51ea0686809b78b6abcc78a4a4fd59eabdac9
ali <ali.7@outlook.com>
2018-11-11 08:54:39 +0200

    change-className-to-use-BEM relate #26

--- client/src/components/common/ProfileCard/index.jsx
+++ client/src/components/common/ProfileCard/index.jsx
@@ -9,7 +9,7 @@ class ProfileCard extends Component {
   render() {
     return (
       <div className="profileCard__main">
-        <img src={avatarImg} alt="avatarImg" className="avatarImg" />
+        <img src={avatarImg} alt="avatarImg" className="avatar__img" />
         <h1 className="profileCard__name">John Doe</h1>
         <h1 className="profileCard__location">Gaza - Palestine</h1>
         <h1 className="profileCard__lang">Arabic - Palestinian</h1>
