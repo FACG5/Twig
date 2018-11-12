90ec911f38563f24428b54b60a9751f9ff026c6e
lubnaabad <lubna_mosa@hotmail.com>
2018-11-12 11:29:24 +0200

    add route TranslationPage components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './components/HomePage';
 import MainPage from './components/MainPage';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import TranslationsPage from './components/TranslationsPage';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Switch>
           <Route exact path="/landing" component={HomePage} />
           <Route exact path="/" component={MainPage} />
+          <Route exact path="/specialization/questions/id" component={TranslationsPage} />
         </Switch>
       </Router>
       <Footer />
