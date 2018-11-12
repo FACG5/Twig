536d97a7a55aeba2f4d3922660f2ed7dc318f4e8
hemasali <hemo3li7@gmail.com>
2018-11-12 11:10:23 +0200

    adding new font awsome icont to library

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  faTimesCircle,
+  faGlobe,
+  faTransgender,
+  faGraduationCap,
+  faMapMarkedAlt,
+  faClipboardList,
+  faIdCard,
+} from '@fortawesome/free-solid-svg-icons';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-library.add(faTimesCircle);
+library.add(
+  faTimesCircle,
+  faGlobe,
+  faTransgender,
+  faGraduationCap,
+  faMapMarkedAlt,
+  faClipboardList,
+  faIdCard,
+);
 /* eslint-disable */
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
