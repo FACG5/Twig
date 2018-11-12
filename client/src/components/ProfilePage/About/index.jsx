ee33627572e0963551750e454fdf1710dc665ad1
hemasali <hemo3li7@gmail.com>
2018-11-12 14:09:00 +0200

    fixing the content of file

--- client/src/components/ProfilePage/About/index.jsx
+++ client/src/components/ProfilePage/About/index.jsx
@@ -1,63 +1,15 @@
-import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import './style.css';
-import About from './About';
-import LanguageLevel from './About/LanguageLevel';
-import ProfileCard from './ProfileCard';
+import React from 'react';
+import Overview from './Overview';
+import AboutMe from './AboutMe';
+import LanguageLevel from './LanguageLevel';
 
-class Profile extends Component {
-  state = {
-    overView: true,
-    languages: false,
-  };
+export default function About(props) {
+  return (
+    <React.Fragment>
+      <Overview />
+      <AboutMe />
+      <LanguageLevel />
+    </React.Fragment>
 
-  shiftTab = () => {
-    this.setState(prev => ({
-      overView: !prev.overView,
-      languages: !prev.languages,
-    }));
-  };
-
-  render() {
-    const { pathname } = this.props.history.location;
-    const { overView, languages } = this.state;
-    return (
-      <Fragment>
-        <div className="profile__main">
-          <div className="card__section">
-            <ProfileCard />
-          </div>
-          <div>
-            <div className="tabs__section">
-              <Link
-                onClick={() => this.shiftTab()}
-                to="/profile"
-                className={`tab__button ${
-                  overView ? 'tab__button--clicked' : null
-                }`}
-              >
-                about
-              </Link>
-              <Link
-                onClick={() => this.shiftTab()}
-                to="/profile/languages"
-                className={`tab__button ${
-                  languages ? 'tab__button--clicked' : null
-                }`}
-              >
-                Translations
-              </Link>
-            </div>
-            {pathname.match('/profile/languages') ? (
-              <LanguageLevel />
-            ) : (
-              <About />
-            )}
-          </div>
-        </div>
-      </Fragment>
-    );
-  }
+  );
 }
-
-export default Profile;
