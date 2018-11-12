d48980e56aed1818f6c95b5b1d87d86e32e03bb0
hemasali <hemo3li7@gmail.com>
2018-11-12 14:11:15 +0200

    destructuring the react Fragment

--- client/src/components/ProfilePage/About/index.jsx
+++ client/src/components/ProfilePage/About/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Overview from './Overview';
 import AboutMe from './AboutMe';
 import LanguageLevel from './LanguageLevel';
 
 export default function About(props) {
   return (
-    <React.Fragment>
+    <Fragment>
       <Overview />
       <AboutMe />
       <LanguageLevel />
-    </React.Fragment>
+    </Fragment>
 
   );
 }
