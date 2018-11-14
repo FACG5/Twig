7d21033e9186b784d4433eada3baa1b7bf138284
lubnaabad <lubna_mosa@hotmail.com>
2018-11-14 21:11:50 +0200

    remove text-decoration in style.css

--- client/src/components/MainPage/SpecializationCard/index.jsx
+++ client/src/components/MainPage/SpecializationCard/index.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const Card = (props) => {
   const { values } = props;
   return (
     <ul className="category__box">
-      {values.map((element) => {
+      {values && values.map((element) => {
         const {
           section, id, questions, imagePath,
         } = element;
         return (
-          <li key={id} className="category__card">
-            <div className="category__card--content">
-              <img src={imagePath} alt="img" className="category__card--img" />
-              <div className="category__card--section">{section}</div>
-              <div className="category__card--content--box">
-                {questions}
+          <Link to={`/${section}`}>
+            <li key={id} className="category__card">
+              <div className="category__card--content">
+                <img src={imagePath} alt="img" className="category__card--img" />
+                <div className="category__card--section">{section}</div>
+                <div className="category__card--content--box">
+                  {questions}
+                </div>
               </div>
-            </div>
-          </li>
+            </li>
+          </Link>
         );
       })}
     </ul>
