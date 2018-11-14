acff20b4ece7612f2c5cb0b885510f4ff9bc2a46
lubnaabad <lubna_mosa@hotmail.com>
2018-11-14 21:15:46 +0200

    put link in QuestionCard

--- client/src/components/QuestionsPage/QuestionCard/index.jsx
+++ client/src/components/QuestionsPage/QuestionCard/index.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const Card = (props) => {
-  const { values } = props;
+  const { values, section } = props;
   return (
     <ul className="box">
-      {values.map((element) => {
+      {values && values.map((element) => {
         const {
           value, id, user, translations, verified,
         } = element;
         return (
-          <li key={id} className="box__card">
-            <div>{value}</div>
-            <div className="box__card--content">
-              <div>
-                {user}
-              </div>
-              <div className="box__card--content--box">
+          <Link to={`/${section}/questions/${id}`}>
+            <li key={id} className="box__card">
+              <div>{value}</div>
+              <div className="box__card--content">
                 <div>
-                  {translations}
+                  {user}
                 </div>
-                <div>
-                  {verified}
+                <div className="box__card--content--box">
+                  <div>
+                    {translations}
+                  </div>
+                  <div>
+                    {verified}
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
+            </li>
+          </Link>
         );
       })}
     </ul>
@@ -35,6 +38,8 @@ const Card = (props) => {
 
 Card.propTypes = {
   values: PropTypes.instanceOf(Array).isRequired,
+  section: PropTypes.string.isRequired,
+
 };
 
 export default Card;
