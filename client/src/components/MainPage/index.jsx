2c003e9e02a4fb1108217160232616d6c62d2ee8
hemasali <hemo3li7@gmail.com>
2018-11-15 11:13:55 +0200

    delete the Router tag from MainPage Component

--- client/src/components/MainPage/index.jsx
+++ client/src/components/MainPage/index.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment, Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import ProgressBar from '../common/ProgressBar';
 import ProfileCard from '../common/ProfileCard';
 import './style.css';
@@ -12,40 +12,28 @@ class MainPage extends Component {
 
   render() {
     return (
-      <Fragment>
-        <Router>
-          <div className="mainpage">
-            <div>
-              <ProfileCard
-                name="Ali ALI"
-                location="Gaza - Palestine"
-                language="Arabic"
-                dialect="Palestinian"
-              />
-              <ProgressBar />
-            </div>
-            <div className="mainpage__content">
-              <Switch>
-                <Route
-                  exact
-                  path="/"
-                  component={Specialization}
-                />
-                <Route
-                  exact
-                  path="/:name"
-                  component={QuestionPage}
-                />
-                <Route
-                  exact
-                  path="/:name/questions/:questionid"
-                  component={TranslationsPage}
-                />
-              </Switch>
-            </div>
-          </div>
-        </Router>
-      </Fragment>
+      <div className="mainpage">
+        <div>
+          <ProfileCard
+            name="Ali ALI"
+            location="Gaza - Palestine"
+            language="Arabic"
+            dialect="Palestinian"
+          />
+          <ProgressBar />
+        </div>
+        <div className="mainpage__content">
+          <Switch>
+            <Route exact path="/" component={Specialization} />
+            <Route exact path="/:name" component={QuestionPage} />
+            <Route
+              exact
+              path="/:name/questions/:questionid"
+              component={TranslationsPage}
+            />
+          </Switch>
+        </div>
+      </div>
     );
   }
 }
