a24bf8feb59311d6aeced35fb49a4c6fdb3bf790
hemasali <hemo3li7@gmail.com>
2018-11-18 11:13:18 +0200

    running the connection in index of server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
+const { connection } = require('./database/models');
 
 const port = app.get('port');
-
-app.listen(port, () => console.log(`the server started on port ${port}`));
+connection.sync().then(() => {
+  app.listen(port, () => console.log(`the server started on port ${port}`));
+});
