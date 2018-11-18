8806b41949a652072e1a3780fc7e890bd7aa1679
hemasali <hemo3li7@gmail.com>
2018-11-18 12:02:44 +0200

    fixing heroku bug

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ const controller = require('./controller');
 const app = express();
 
 app.disable('x-powered-by');
+console.log(process.env.PORT);
+
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
