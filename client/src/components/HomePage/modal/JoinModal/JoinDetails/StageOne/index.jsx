8044ad2d651d5226c8ad08a1a39b15bb11b7faf2
hemasali <hemo3li7@gmail.com>
2018-11-18 09:54:39 +0200

    using context consumer in stage one and stage two

--- client/src/components/HomePage/modal/JoinModal/JoinDetails/StageOne/index.jsx
+++ client/src/components/HomePage/modal/JoinModal/JoinDetails/StageOne/index.jsx
@@ -1,82 +1,96 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Button from '../../../../../common/Button';
 import Inputs from '../../../../../common/Inputs';
 import './style.css';
+import { ModalConsumer } from '../../../ModalContext';
 
-export default function StageOne(props) {
-  const {
-    onChange, changeStage, backFromDetails,
-  } = props;
+export default function StageOne() {
   return (
-    <div className="modal__details">
-      <h4 className="title"> Tell us some details about your language skills </h4>
-      <hr />
-      <div className="checkbox__container">
-        <label className="container__checkbox">
-          I’m a native speaker / mothertongue.
-          <Inputs
-            className=""
-            placeholder=""
-            id="native"
-            onChange={onChange}
-            type="checkbox"
-            name="native"
+    <ModalConsumer>
+      {context => (
+        <div className="modal__details">
+          <div className="modalHead">
+            <h4 className="title">
+              Tell us some details about your language skills
+            </h4>
+            <FontAwesomeIcon
+              icon="times-circle"
+              className="closeButton"
+              onClick={context.closeModel}
+            />
+          </div>
+          <hr />
+          <div className="checkbox__container">
+            <label className="container__checkbox">
+              I’m a native speaker / mothertongue.
+              <Inputs
+                className=""
+                placeholder=""
+                id="native"
+                onChange={context.storeValue}
+                type="checkbox"
+                name="native"
+              />
+              <span className="span__checkbox" />
+            </label>
+            <label className="container__checkbox">
+              I have tested at upper intermediate or advanced level.
+              <Inputs
+                className=""
+                placeholder=""
+                onChange={context.storeValue}
+                type="checkbox"
+                name="intemediate"
+                id="intemediate"
+              />
+              <span className="span__checkbox" />
+            </label>
+            <label className="container__checkbox">
+              I have completed University or professional training this
+              language.
+              <Inputs
+                className=""
+                placeholder=""
+                onChange={context.storeValue}
+                type="checkbox"
+                name="university"
+              />
+              <span className="span__checkbox" />
+            </label>
+            <label className="container__checkbox">
+              I’m self-taught.
+              <Inputs
+                className=""
+                placeholder=""
+                onChange={context.storeValue}
+                type="checkbox"
+                name="self"
+              />
+              <span className="span__checkbox" />
+            </label>
+            <Inputs
+              className="input__other"
+              placeholder="Write here if you have other details .."
+              onChange={context.storeValue}
+              type="text"
+              name="otherSkills"
+            />
+          </div>
+          <Button
+            className="button__next"
+            id="nextDetails"
+            onClick={context.changeStage}
+            value="Next"
           />
-          <span className="span__checkbox" />
-        </label>
-        <label className="container__checkbox">
-          I have tested at upper intermediate or advanced level.
-          <Inputs
-            className=""
-            placeholder=""
-            onChange={onChange}
-            type="checkbox"
-            name="intemediate"
-            id="intemediate"
+          <Button
+            className="button__back"
+            id="nextDetails"
+            onClick={context.backFromDetails}
+            value="Back"
           />
-          <span className="span__checkbox" />
-        </label>
-        <label className="container__checkbox">
-          I have completed University or professional training this language.
-          <Inputs
-            className=""
-            placeholder=""
-            onChange={onChange}
-            type="checkbox"
-            name="university"
-          />
-          <span className="span__checkbox" />
-        </label>
-        <label className="container__checkbox">
-          I’m self-taught.
-          <Inputs
-            className=""
-            placeholder=""
-            onChange={onChange}
-            type="checkbox"
-            name="self"
-          />
-          <span className="span__checkbox" />
-        </label>
-      </div>
-      <Button
-        className="button__next"
-        id="nextDetails"
-        onClick={changeStage}
-        value="Next"
-      />
-      <Button
-        className="button__back"
-        id="nextDetails"
-        onClick={backFromDetails}
-        value="Back"
-      />
-    </div>
+        </div>
+      )}
+    </ModalConsumer>
   );
 }
-StageOne.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  changeStage: PropTypes.func.isRequired,
-  backFromDetails: PropTypes.func.isRequired,
-};
