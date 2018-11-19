688cec402dbb670a815020adaab4da5dd65f6c2f
hemasali <hemo3li7@gmail.com>
2018-11-19 10:28:10 +0200

    editing the login component and connect it with backend

--- client/src/components/HomePage/modal/LoginModal/index.jsx
+++ client/src/components/HomePage/modal/LoginModal/index.jsx
@@ -2,76 +2,109 @@ import React, { Component } from 'react';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Input from '../../../common/Inputs';
 import Button from '../../../common/Button';
+import { ModalConsumer } from '../ModalContext';
+import PopUp from '../../../common/PopUp';
 
 class Login extends Component {
   state = {};
 
   onChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => {});
   };
 
-  onClick = () => {
-    const { history, closeModel } = this.props;
-    history.push('/');
-    closeModel();
+  onClick = async (context) => {
+    const { history } = this.props;
+    const { loginEmail, loginPassword } = this.state;
+    if (!loginEmail || !loginPassword) {
+      context.setPopUpMessage({
+        message: 'Please fill all of the fields !',
+        title: 'Error !',
+      });
+    } else {
+      try {
+        const loginData = { loginEmail, loginPassword };
+        const loginResult = await axios.post('/login', loginData);
+        if (loginResult.status === 200) {
+          history.push('/');
+          context.closeModel();
+        }
+      } catch (error) {
+        const { data } = error.response;
+        context.setPopUpMessage({ message: data, title: 'Error !' });
+      }
+    }
   };
 
   render() {
-    const { switchModel, closeModel } = this.props;
-
     return (
-      <div className="modal">
-        <div className="modal__content">
-          <div className="modalHead">
-            <h1>Login To TWIG</h1>
-            <FontAwesomeIcon
-              icon="times-circle"
-              className="closeButton"
-              onClick={closeModel}
-            />
+      <ModalConsumer>
+        {context => (
+          <div className="modal">
+            <div className="modal__content">
+              <div className="modalHead">
+                <h1>Login To TWIG</h1>
+                <FontAwesomeIcon
+                  icon="times-circle"
+                  className="closeButton"
+                  onClick={context.closeModel}
+                />
+              </div>
+              <hr />
+              <div className="content">
+                <Input
+                  name="loginEmail"
+                  type="email"
+                  className="input__email"
+                  placeholder="Enter your email"
+                  onChange={this.onChange}
+                  value={context.loginEmail}
+                />
+                <Input
+                  name="loginPassword"
+                  type="password"
+                  className="input__password"
+                  placeholder="Enter your password"
+                  onChange={this.onChange}
+                  value={context.loginPassword}
+                />
+                <Button
+                  className="button__login"
+                  value="Login"
+                  onClick={() => this.onClick(context)}
+                />
+                <h3>Or</h3>
+                <Button
+                  className="button__linkedin"
+                  value="Continue with LinkedIn"
+                  onClick={null}
+                />
+              </div>
+              <hr />
+              <h3>Don&apos;t Have an accout ?</h3>
+              <Button
+                className="joinButton"
+                value="Join"
+                onClick={context.switchModel}
+              />
+            </div>
+            {context.popUpMessage ? (
+              <PopUp
+                title={context.popUpMessage.title}
+                message={context.popUpMessage.message}
+                closePopUp={context.closePopUp}
+              />
+            ) : null}
           </div>
-          <hr />
-          <div className="content">
-            <Input
-              name="email"
-              type="email"
-              className="input__email"
-              placeholder="Enter your email"
-              onChange={this.onChange}
-            />
-            <Input
-              name="password"
-              type="password"
-              className="input__password"
-              placeholder="Enter your password"
-              onChange={this.onChange}
-            />
-            <Button
-              className="button__login"
-              value="Login"
-              onClick={this.onClick}
-            />
-            <h3>Or</h3>
-            <Button
-              className="button__linkedin"
-              value="Continue with LinkedIn"
-              onClick={null}
-            />
-          </div>
-          <hr />
-          <h3>Don&apos;t Have an accout ?</h3>
-          <Button className="joinButton" value="Join" onClick={switchModel} />
-        </div>
-      </div>
+        )}
+      </ModalConsumer>
     );
   }
 }
 Login.propTypes = {
-  closeModel: PropTypes.func.isRequired,
-  switchModel: PropTypes.func.isRequired,
   history: PropTypes.instanceOf(Object).isRequired,
 };
 export default Login;
