3f07a88187436948abc55c8024fe4fc0c485d54c
lubnaabad <lubna_mosa@hotmail.com>
2018-11-20 20:22:44 +0200

    add getTranslations function

---
+++ server/database/query/translations.js
@@ -0,0 +1,10 @@
+const sequelize = require('../config/index');
+
+const getTranslations = async (name) => {
+  const questionsData = await sequelize.query(
+    'select count(translations.question_id) As translations,questions.* ,speclalizations.*,users.first_name As username from questions left join translations on questions.id = translations.question_id join speclalizations on questions.speclalization_id = speclalizations.id join users on questions.owner = users.id  where speclalizations.name = :name GROUP BY questions.id ,users.id,speclalizations.id', { replacements: { name } },
+  );
+
+  return questionsData;
+};
+module.exports = getTranslations;
