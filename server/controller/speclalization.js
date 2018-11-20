1bf41ca5018977c443209256f293e1acda76d1ad
ali <ali.7@outlook.com>
2018-11-20 12:15:00 +0200

    refactor-getSpeclalizations-function relate #160

--- server/controller/speclalization.js
+++ server/controller/speclalization.js
@@ -3,10 +3,10 @@ const getSpeclalizations = require('../database/query/speclalizations');
 exports.get = async (request, response) => {
   try {
     const result = await getSpeclalizations();
-    if (result === []) {
+    if (result[0] === []) {
       response.send('There\'s no specialty in database !');
     } else {
-      response.send(result);
+      response.send(result[0]);
     }
   } catch (error) {
     response.status(500).send('Server Error');
