4d9e9f49152436201ca00ca9dad65aa3aa20a226
ali <ali.7@outlook.com>
2018-11-20 14:39:14 +0200

    remove-force-true-from-db

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const app = require('./app');
 const { connection } = require('./database/models');
 
 const port = app.get('port');
-connection.sync({
-  force:true
-}).then(() => {
+connection.sync().then(() => {
   app.listen(port, () => console.log(`the server started on port ${port}`));
 });
