c267c00e5442138763918b6e12a54ff230f149a3
ali <ali.7@outlook.com>
2018-11-20 03:15:21 +0200

    create-get-function-to-handle-getSpeclalizations-query relates #160

---
+++ server/controller/speclalization.js
@@ -0,0 +1,14 @@
+const getSpeclalizations = require('../database/query/getSpeclalizations');
+
+exports.get = async (request, response) => {
+  try {
+    const result = await getSpeclalizations();
+    if (result === []) {
+      response.send('There\'s no specialty in database !');
+    } else {
+      response.send(result);
+    }
+  } catch (error) {
+    response.status(500).send('Server Error');
+  }
+};
