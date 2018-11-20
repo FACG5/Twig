5b9bc27dd5d94a4dc51a087fc6a59f6e4662610a
ali <ali.7@outlook.com>
2018-11-20 13:59:02 +0200

    force-to-frop-db-tables

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const app = require('./app');
 const { connection } = require('./database/models');
 
 const port = app.get('port');
-connection.sync().then(() => {
+connection.sync({
+  force:true
+}).then(() => {
   app.listen(port, () => console.log(`the server started on port ${port}`));
 });
