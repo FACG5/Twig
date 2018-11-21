402dd3506fac982568f0791fbfac48e74b27e825
ali <ali.7@outlook.com>
2018-11-21 14:46:05 +0200

    modify-quey-param-to-userId relate #81

--- server/database/query/userDetails.js
+++ server/database/query/userDetails.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config');
 
-const getUserDetails = async (userID) => {
+const getUserDetails = async (userId) => {
   const userDetails = await sequelize.query(
-    'select users.first_name AS firstName, users.last_name AS lastName, users.id AS userId, users.location AS location, users.avatar_url AS avatarUrl,dialects.name AS dialectName, languages.name AS languageName from users join dialects on dialects.id = users.dialect_id join languages on languages.id = users.language_id  where users.id = :userId', { replacements: { userID } },
+    'select users.first_name AS firstName, users.last_name AS lastName, users.id AS userId, users.location AS location, users.avatar_url AS avatarUrl,dialects.name AS dialectName, languages.name AS languageName from users join dialects on dialects.id = users.dialect_id join languages on languages.id = users.language_id  where users.id = :userId', { replacements: { userId } },
   );
   return userDetails;
 };
