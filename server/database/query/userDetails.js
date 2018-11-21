29d87951301cb69ef13f244e900675ded0a101d4
ali <ali.7@outlook.com>
2018-11-21 15:59:59 +0200

    add-qoutes-to-naming-of-varaibles-in-query

--- server/database/query/userDetails.js
+++ server/database/query/userDetails.js
@@ -2,7 +2,7 @@ const sequelize = require('../config');
 
 const getUserDetails = async (userId) => {
   const userDetails = await sequelize.query(
-    'select users.first_name AS firstName, users.last_name AS lastName, users.id AS userId, users.location AS location, users.avatar_url AS avatarUrl,dialects.name AS dialectName, languages.name AS languageName from users join dialects on dialects.id = users.dialect_id join languages on languages.id = users.language_id  where users.id = :userId', { replacements: { userId } },
+    'select users.first_name AS "firstName", users.last_name AS "lastName", users.id AS "userId", users.location AS location, users.avatar_url AS "avatarUrl" ,dialects.name AS "dialectName", languages.name AS "languageName" from users join dialects on dialects.id = users.dialect_id join languages on languages.id = users.language_id  where users.id = :userId', { replacements: { userId } },
   );
   return userDetails;
 };
