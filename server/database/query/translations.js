1095744ff350a9cfe953bc6740e00487bc53d8d3
lubnaabad <lubna_mosa@hotmail.com>
2018-11-21 08:51:13 +0200

    edit query get translations

--- server/database/query/translations.js
+++ server/database/query/translations.js
@@ -1,10 +1,11 @@
 const sequelize = require('../config/index');
 
-const getTranslations = async (name) => {
-  const questionsData = await sequelize.query(
-    'select count(translations.question_id) As translations,questions.* ,speclalizations.*,users.first_name As username from questions left join translations on questions.id = translations.question_id join speclalizations on questions.speclalization_id = speclalizations.id join users on questions.owner = users.id  where speclalizations.name = :name GROUP BY questions.id ,users.id,speclalizations.id', { replacements: { name } },
+const getTranslations = async (questionId, userId) => {
+  const translationsData = await sequelize.query(
+    'select translations.id As translation_id,translations.vote_down As vote_down,translations.vote_up As vote_up,questions.questions,questions.id,users.first_name As username ,users.avatar_url,users.language_id,users.dialect_id,translations.translation from questions left join translations on questions.id =translations.question_id  join users on questions.owner = users.id where (select users.dialect_id from users where users.id =:userId)= translations.dialect_id and questions.id =:questionId GROUP BY questions.id ,users.id,translations.id',
+    { replacements: { questionId, userId } },
   );
 
-  return questionsData;
+  return translationsData;
 };
 module.exports = getTranslations;
