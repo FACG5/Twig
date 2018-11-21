b30778a6a3fdedcf7fd9367c2a11c98be7119a5a
ali <ali.7@outlook.com>
2018-11-21 16:01:41 +0200

    send-obj-to-frontend-insted-of-array-of-obj

--- server/controller/user.js
+++ server/controller/user.js
@@ -4,7 +4,7 @@ exports.get = async (request, response) => {
   try {
     const userId = request.id;
     const result = await getUserDetails(userId);
-    response.send(result[0]);
+    response.send(result[0][0]);
   } catch (error) {
     response.status(500).send('Server Error');
   }
