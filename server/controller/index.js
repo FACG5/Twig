607de157343ca3daf7d2a84562be704ca882c843
lubnaabad <lubna_mosa@hotmail.com>
2018-11-21 08:49:10 +0200

    add middelwire authentications in routers

--- server/controller/index.js
+++ server/controller/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const login = require('./login');
 const questions = require('./questions');
 const specialization = require('./speclalization');
+const translation = require('./translations');
+const checkToken = require('./authentication/index');
 
 const router = express.Router();
 
 router.post('/login', login.post);
+
+router.use(checkToken);
 router.get('/specialization/:section', questions.get);
 router.get('/specialization', specialization.get);
+router.get('/questions/:questionId', translation.get);
 
 module.exports = router;
