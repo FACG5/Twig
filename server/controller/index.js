4bd53d500f0395a92eb8a55acb935aa2a394b386
hemasali <hemo3li7@gmail.com>
2018-11-21 13:11:07 +0200

    adding the routes of signup process

--- server/controller/index.js
+++ server/controller/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const login = require('./login');
 const questions = require('./questions');
 const specialization = require('./speclalization');
+const skills = require('./skills');
+const jobs = require('./jobs');
+const signup = require('./signup');
 
 const router = express.Router();
 
 router.post('/login', login.post);
 router.get('/specialization/:section', questions.get);
 router.get('/specialization', specialization.get);
+router.get('/get-skills', skills.get);
+router.get('/get-jobs', jobs.get);
+router.post('/signup', signup.post);
+
 
 module.exports = router;
