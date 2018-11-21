929b6376f6ea9d75023d1132add1628548cbeeda
hemasali <hemo3li7@gmail.com>
2018-11-21 22:43:58 +0200

    add the routes of get language and get dialects

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,14 +4,19 @@ const questions = require('./questions');
 const specialization = require('./speclalization');
 const translation = require('./translations');
 const checkToken = require('./authentication');
+const languages = require('./languages');
+const dialects = require('./dialects');
 
 const router = express.Router();
 
 router.post('/login', login.post);
+router.get('/questions/:questionId', translation.get);
+router.get('/get-languages', languages.get);
+router.get('/get-dialcets/:languageId', dialects.get);
 
 router.use(checkToken);
 router.get('/specialization/:section', questions.get);
 router.get('/specialization', specialization.get);
-router.get('/questions/:questionId', translation.get);
+
 
 module.exports = router;
