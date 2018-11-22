36461f78a602326397123ffa566d64b9b4a8c7e1
ali <ali.7@outlook.com>
2018-11-22 10:33:27 +0200

    resolove-conflict-in-index-controlle

--- server/controller/index.js
+++ server/controller/index.js
@@ -7,12 +7,9 @@ const jobs = require('./jobs');
 const signup = require('./signup');
 const translation = require('./translations');
 const checkToken = require('./authentication');
-<<<<<<< HEAD
 const userDetails = require('./user');
-=======
 const languages = require('./languages');
 const dialects = require('./dialects');
->>>>>>> e2e4b0f41c6db4db7340b931f97c9cf9c5bd3e10
 
 const router = express.Router();
 
@@ -27,11 +24,7 @@ router.use(checkToken);
 router.get('/questions/:questionId', translation.get);
 router.get('/specialization/:section', questions.get);
 router.get('/specialization', specialization.get);
-<<<<<<< HEAD
 router.get('/questions/:questionId', translation.get);
 router.get('/details', userDetails.get);
-=======
-
->>>>>>> e2e4b0f41c6db4db7340b931f97c9cf9c5bd3e10
 
 module.exports = router;
