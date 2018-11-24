7c3f4d0a5c63e59b9d01eb8cf1ab91895691634a
hemasali <hemo3li7@gmail.com>
2018-11-24 19:33:15 +0200

    creating the Main page  layout which contain Header, Footer, Profile card

---
+++ client/src/components/layouts/MainLayout/index.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import Header from '../../common/Header';
+import Footer from '../../common/Footer';
+import ProgressBar from '../../common/ProgressBar';
+import ProfileCard from '../../common/ProfileCard';
+
+
+export default function MainLayout(props) {
+  const { component: Component, ...rest } = props;
+
+  return (
+    <Fragment>
+      <Header />
+      <div className="mainpage">
+        <div>
+          <ProfileCard />
+          <ProgressBar />
+        </div>
+        <div className="mainpage__content">
+          <Route {...rest} component={Component} />
+        </div>
+      </div>
+      <Footer />
+    </Fragment>
+  );
+}
