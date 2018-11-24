53e8fbc0430e39f702e06c0f257302925d93bcba
hemasali <hemo3li7@gmail.com>
2018-11-25 00:23:38 +0200

    adding the link on profile button to redirect to profile page

--- client/src/components/common/Header/LoggedinButtons/index.jsx
+++ client/src/components/common/Header/LoggedinButtons/index.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Button from '../../Button';
 
 
@@ -7,7 +8,9 @@ export default function LoggedinButtons(props) {
   const { logout } = props;
   return (
     <Fragment>
-      <Button value="My profile" className="profile__button" />
+      <Link to="/profile">
+        <Button value="My profile" className="profile__button" />
+      </Link>
       <Button value="Log out" className="logout__button" onClick={logout} />
     </Fragment>
   );
