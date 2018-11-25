bd4979729d60f36e9c3dbd1bbab68111b254edd0
hemasali <hemo3li7@gmail.com>
2018-11-25 10:00:40 +0200

    check the cookie in mainLayout if exist or not , to avoid send request to server if not exist

--- client/src/components/layouts/MainLayout/index.jsx
+++ client/src/components/layouts/MainLayout/index.jsx
@@ -1,27 +1,47 @@
-import React, { Fragment } from 'react';
-import { Route } from 'react-router-dom';
+import Cookie from 'js-cookie';
+import React, { Fragment, Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
 import Header from '../../common/Header';
 import Footer from '../../common/Footer';
 import ProgressBar from '../../common/ProgressBar';
 import ProfileCard from '../../common/ProfileCard';
 import './style.css';
+import Loading from '../../common/Loading';
 
-export default function MainLayout(props) {
-  const { component: Component, ...rest } = props;
+class MainLayout extends Component {
+  state = {};
 
-  return (
-    <Fragment>
-      <Header />
-      <div className="mainpage">
-        <div>
-          <ProfileCard />
-          <ProgressBar />
+  componentDidMount() {
+    const { history } = this.props;
+    const jwtCooke = Cookie.get('jwt');
+    if (jwtCooke) {
+      this.setState({ login: true });
+    } else {
+      history.push('/');
+    }
+  }
+
+  render() {
+    const { component, ...rest } = this.props;
+    const { login } = this.state;
+    return login ? (
+      <Fragment>
+        <Header />
+        <div className="mainpage">
+          <div>
+            <ProfileCard />
+            <ProgressBar />
+          </div>
+          <div className="mainpage__content">
+            <Route {...rest} component={component} />
+          </div>
         </div>
-        <div className="mainpage__content">
-          <Route {...rest} component={Component} />
-        </div>
-      </div>
-      <Footer />
-    </Fragment>
-  );
+        <Footer />
+      </Fragment>
+    ) : (
+      <Loading />
+    );
+  }
 }
+
+export default withRouter(MainLayout);
