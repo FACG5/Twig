03c0c5aaeeac23efa5a9b6ad1dc33f31cc903d1d
ali <ali.7@outlook.com>
2018-11-25 12:40:24 +0200

    refactor-LanguageLevel-section-to-get-data-from-db relate #227

--- client/src/components/ProfilePage/About/LanguageLevel/index.jsx
+++ client/src/components/ProfilePage/About/LanguageLevel/index.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './style.css';
 
-export default function LanguageLevel() {
-  return (
-    <div className="language__box">
-      <h3 className="language__title">Language Level</h3>
-      <hr />
-      <div className="language__content">
-        <ul>
-          <li> I’m a native speaker / mothertongue. </li>
-          <li> I have tested at upper intermediate or advanced level. </li>
-          <li> I have completed University or professional training this language.  </li>
-          <li> I’m self-taught. </li>
-        </ul>
+class LanguageLevel extends Component {
+  state = {
+    values: [],
+  };
+
+  componentWillMount() {
+    axios.get('/api/v1/profile').then((res) => {
+      const results = res.data;
+      const { languageResult } = results;
+      this.setState({ values: languageResult });
+    }).catch((error) => {
+      const { status } = error.response;
+      if (status === 404) {
+        this.setState({ message: 'Page Not Found' });
+      }
+    });
+  }
+
+  render() {
+    const { values } = this.state;
+    const result = values.map((element) => {
+      const description = element.skillsDescription;
+      const id = element.skillsId;
+      return <li key={id}>{description}</li>;
+    });
+
+    return (
+      <div className="language__box">
+        <h3 className="language__title">Language Level</h3>
+        <hr />
+        <div className="language__content">
+          <ul>
+            {result}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default LanguageLevel;
