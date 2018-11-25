c786f5c68ddb83f1b678a63bd71570f4eab5c44d
ali <ali.7@outlook.com>
2018-11-25 14:45:29 +0200

    remove-ProfileCard-from-profile-page relate #235

--- client/src/components/ProfilePage/index.jsx
+++ client/src/components/ProfilePage/index.jsx
@@ -3,7 +3,6 @@ import Button from '../common/Button';
 import './style.css';
 import About from './About';
 import LanguageLevel from './About/LanguageLevel';
-import ProfileCard from './ProfileCard';
 
 class Profile extends Component {
   state = {
@@ -22,29 +21,24 @@ class Profile extends Component {
     const { overView, languages } = this.state;
     return (
       <Fragment>
-        <div className="profile__main">
-          <div className="card__section">
-            <ProfileCard />
+        <div>
+          <div className="tabs__section">
+            <Button
+              onClick={this.shiftTab}
+              className={`tab__button ${
+                overView ? 'tab__button--clicked' : null
+              }`}
+              value="About"
+            />
+            <Button
+              onClick={this.shiftTab}
+              className={`tab__button ${
+                languages ? 'tab__button--clicked' : null
+              }`}
+              value="Translations"
+            />
           </div>
-          <div>
-            <div className="tabs__section">
-              <Button
-                onClick={this.shiftTab}
-                className={`tab__button ${
-                  overView ? 'tab__button--clicked' : null
-                }`}
-                value="About"
-              />
-              <Button
-                onClick={this.shiftTab}
-                className={`tab__button ${
-                  languages ? 'tab__button--clicked' : null
-                }`}
-                value="Translations"
-              />
-            </div>
-            {languages ? <LanguageLevel /> : <About />}
-          </div>
+          {languages ? <LanguageLevel /> : <About />}
         </div>
       </Fragment>
     );
