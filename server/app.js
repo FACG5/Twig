9d0147ffe4e9e20b1371f3a6ace65a4376216b97
hemasali <hemo3li7@gmail.com>
2018-11-26 22:29:23 +0200

    using the express-fileupload in app

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const fileUpload = require('express-fileupload');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -8,11 +9,12 @@ const app = express();
 app.disable('x-powered-by');
 
 app.set('port', process.env.PORT || 5000);
-
+app.use(fileUpload());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use('/api/v1', controller);
 
