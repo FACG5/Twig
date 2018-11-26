74c95343c7b7484427de4c963f2250a854e6bc80
hemasali <hemo3li7@gmail.com>
2018-11-26 11:06:16 +0200

    editing the index of loading modal

--- client/src/components/HomePage/modal/LoadingModal/index.jsx
+++ client/src/components/HomePage/modal/LoadingModal/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 
 export default function LoadingModal() {
   return (
-    <div className="loading__main">
+    <div className="loading__modal">
       <div className="spinner__modal">
         <div className="double-bounce1" />
         <div className="double-bounce2" />
