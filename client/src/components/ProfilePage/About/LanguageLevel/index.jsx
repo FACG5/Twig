14b014fe0996dba79e5e13d622e450952f1c404d
hemasali <hemo3li7@gmail.com>
2018-11-26 11:04:47 +0200

    recieve profile data in language level

--- client/src/components/ProfilePage/About/LanguageLevel/index.jsx
+++ client/src/components/ProfilePage/About/LanguageLevel/index.jsx
@@ -1,58 +1,31 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
-class LanguageLevel extends Component {
-  state = {
-    values: [],
-  };
+const LanguageLevel = (props) => {
+  const { languageResult } = props;
 
-  componentWillMount() {
-    axios.get('/api/v1/profile').then((res) => {
-      const results = res.data;
-      const { languageResult } = results;
-      this.setState({ values: languageResult });
-    }).catch((error) => {
-      const { status } = error.response;
-      if (status === 404) {
-        this.setState({ message: 'Page Not Found' });
-      }
-    });
-  }
-
-  render() {
-    const { values } = this.state;
-    if (values.length === 0) {
-      return (
-        <div className="language__box">
-          <h3 className="language__title">Language Level</h3>
-          <div className="language__content-notfound">
-            <hr />
-            <ul>
-              <li>No Language Level Specified !</li>
-            </ul>
-          </div>
-        </div>
-      );
-    }
-    const result = values.map((element) => {
-      const description = element.skillsDescription;
-      const id = element.skillsId;
-      return <li key={id}>{description}</li>;
-    });
-
-    return (
-      <div className="language__box">
-        <h3 className="language__title">Language Level</h3>
+  return (
+    <div className="language__box">
+      <h3 className="language__title">Language Level</h3>
+      <div className="language__content">
         <hr />
-        <div className="language__content">
-          <ul>
-            {result}
-          </ul>
-        </div>
+        <ul>
+          {languageResult.length ? (
+            languageResult.map((element) => {
+              const { skillsDescription, skillsId } = element;
+              return <li key={skillsId}>{skillsDescription}</li>;
+            })
+          ) : (
+            <h4> No Languages level Specified !</h4>
+          )}
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+LanguageLevel.propTypes = {
+  languageResult: PropTypes.instanceOf(Array).isRequired,
+};
 
 export default LanguageLevel;
