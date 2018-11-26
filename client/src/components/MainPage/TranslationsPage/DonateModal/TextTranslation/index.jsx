eadb12caced4868191910f09e173e6547e0d7f4b
lubnaabad <lubna_mosa@hotmail.com>
2018-11-26 12:42:41 +0200

    call onChange and onClick from props

--- client/src/components/MainPage/TranslationsPage/DonateModal/TextTranslation/index.jsx
+++ client/src/components/MainPage/TranslationsPage/DonateModal/TextTranslation/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 import Button from '../../../../common/Button';
 
-export default function TextTranslation() {
+export default function TextTranslation(props) {
+  const { onChange, onClick } = props;
+  const typeId = 1;
+
   return (
     <div className="donate__text">
       <h3 className="textarea__titel"> Write here your translation </h3>
@@ -12,8 +16,13 @@ export default function TextTranslation() {
         id=""
         cols="50"
         rows="4"
+        onChange={onChange}
       />
-      <Button onClick={null} value="Submit Translation" className="donate__submit" />
+      <Button onClick={() => onClick(typeId)} value="Submit Translation" className="donate__submit" />
     </div>
   );
 }
+TextTranslation.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
