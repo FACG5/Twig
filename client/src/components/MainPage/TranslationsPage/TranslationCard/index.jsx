98078d5063470cdd3933469e6da4f192327f28bf
hemasali <hemo3li7@gmail.com>
2018-11-27 12:31:11 +0200

    view the video or audio file in translation card

--- client/src/components/MainPage/TranslationsPage/TranslationCard/index.jsx
+++ client/src/components/MainPage/TranslationsPage/TranslationCard/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronCircleUp, faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronCircleUp,
+  faChevronCircleDown,
+} from '@fortawesome/free-solid-svg-icons';
 
 import './style.css';
 
@@ -9,44 +12,76 @@ const Card = (props) => {
   const { values, voteUpClick, voteDownClick } = props;
   return (
     <ul className="traslations__list">
-      {values && values.map((element) => {
-        const {
-          translation, id, username, date,
-          voteDown,
-          voteUp,
-          avatar_url: avatarUrl,
-        } = element;
-        return (
-          <li key={id} className="traslation__item">
-            <div>
-              <img src={avatarUrl} className="box__img" alt="" />
-              <p>{username}</p>
-            </div>
-            <div className="traslation__container">
+      {values
+        && values.map((element) => {
+          const {
+            translation,
+            id,
+            username,
+            date,
+            voteDown,
+            voteUp,
+            avatar_url: avatarUrl,
+            type_id: typeId,
+            link,
+          } = element;
+          return (
+            <li key={id} className="traslation__item">
               <div>
-                {translation}
+                <img src={avatarUrl} className="box__img" alt="" />
+                <p>{username}</p>
               </div>
-              <div className="traslation__date">
-                {date.slice(0, 10)}
-              </div>
-            </div>
-            <div className="traslation__verify">
-              <div>
-                <div><FontAwesomeIcon icon={faChevronCircleUp} size="lg" className="fa__check--circle" onClick={() => { voteUpClick(id); }} /></div>
+              <div className="traslation__container">
                 <div>
-                  {`${voteUp} vote Up`}
+                  {translation}
+                  {typeId === 2 ? (
+                    <audio controls>
+                      <source src={`/api/v1/files/${link}`} type="audio/mp3" />
+                      <source src={`/api/v1/files/${link}`} type="audio/ogg" />
+                      <p>Your browser doesnt support HTML5 audio.</p>
+                    </audio>
+                  ) : null}
+                  {typeId === 3 ? (
+                    <video width="420" height="240" controls>
+                      <source src={`/api/v1/files/${link}`} type="audio/mp3" />
+                      <source src={`/api/v1/files/${link}`} type="audio/ogg" />
+                      <p>Your browser doesnt support HTML5 audio.</p>
+                    </video>
+                  ) : null}
+                </div>
+                <div className="traslation__date">{date.slice(0, 10)}</div>
+              </div>
+              <div className="traslation__verify">
+                <div>
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faChevronCircleUp}
+                      size="lg"
+                      className="fa__check--circle"
+                      onClick={() => {
+                        voteUpClick(id);
+                      }}
+                    />
+                  </div>
+                  <div>{`${voteUp} vote Up`}</div>
+                </div>
+                <div>
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faChevronCircleDown}
+                      size="lg"
+                      className="fa__times--circle"
+                      onClick={() => {
+                        voteDownClick(id);
+                      }}
+                    />
+                  </div>
+                  <div>{`${voteDown} vote Down`}</div>
                 </div>
               </div>
-              <div>
-                <div>
-                  <FontAwesomeIcon icon={faChevronCircleDown} size="lg" className="fa__times--circle" onClick={() => { voteDownClick(id); }} />
-                </div>
-                <div>{`${voteDown} vote Down`}</div>
-              </div>
-            </div>
-          </li>
-        );
-      })}
+            </li>
+          );
+        })}
     </ul>
   );
 };
