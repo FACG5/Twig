c8d66ec2a34f30bf06a1f94c38d4cce75fabd457
hemasali <hemo3li7@gmail.com>
2018-11-27 11:05:59 +0200

    order the result by editing the query

--- server/database/query/translations.js
+++ server/database/query/translations.js
@@ -6,10 +6,11 @@ const sequelize = require('../config');
 
 const getTranslations = async (questionId, userId) => {
   const questionsData = await sequelize.query(
-    'select questions.* ,users.first_name As username from questions join users on questions.owner = users.id  where questions.id = :questionId GROUP BY questions.id ,users.id', { replacements: { questionId } },
+    'select questions.* ,users.first_name As username from questions join users on questions.owner = users.id  where questions.id = :questionId GROUP BY questions.id ,users.id',
+    { replacements: { questionId } },
   );
   const translationsData = await sequelize.query(
-    'select sum(COALESCE(votes.vote_up, 0)) as "voteUp", sum(COALESCE(votes.vote_down, 0)) as "voteDown",translations.*,users.first_name As username ,users.avatar_url from translations left join votes ON votes.translations_id = translations.id join questions on translations.question_id = questions.id join users on translations.owner = users.id where (select users.dialect_id from users where users.id =:userId)= translations.dialect_id and questions.id =:questionId GROUP BY questions.id ,users.id,translations.id',
+    'select sum(COALESCE(votes.vote_up, 0)) as "voteUp", sum(COALESCE(votes.vote_down, 0)) as "voteDown",translations.*,users.first_name As username ,users.avatar_url from translations left join votes ON votes.translations_id = translations.id join questions on translations.question_id = questions.id join users on translations.owner = users.id where (select users.dialect_id from users where users.id =:userId)= translations.dialect_id and questions.id =:questionId GROUP BY questions.id ,users.id,translations.id order by translations.date desc',
     { replacements: { questionId, userId } },
   );
   const result = { questionsData, translationsData };
