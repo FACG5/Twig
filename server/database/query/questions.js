cb8a452e83b311e8785b44db99c9f3ee6c9337e3
lubnaabad <lubna_mosa@hotmail.com>
2018-12-04 15:18:28 +0200

    add space before As

--- server/database/query/questions.js
+++ server/database/query/questions.js
@@ -5,7 +5,7 @@ const getQuestions = async (name, userId) => {
   const { dialect_id } = dialect[0][0];
 
   const questionsData = await sequelize.query(
-    'select count(translations.dialect_id )As translations,(COALESCE(translations.dialect_id, 0)) AS dialect ,questions.*,speclalizations.name ,speclalizations.id As "speclalizationsId",speclalizations.avatar_url,users.first_name As username from questions left join translations on questions.id = translations.question_id join speclalizations on questions.speclalization_id = speclalizations.id join users on questions.owner = users.id  where  speclalizations.name = :name and translations.dialect_id IS NULL OR translations.dialect_id =:dialect_id GROUP BY questions.id ,users.id,speclalizations.id,translations.dialect_id ORDER BY questions.date DESC ', { replacements: { name, dialect_id } },
+    'select count(translations.dialect_id ) As translations,(COALESCE(translations.dialect_id, 0)) AS dialect ,questions.*,speclalizations.name ,speclalizations.id As "speclalizationsId",speclalizations.avatar_url,users.first_name As username from questions left join translations on questions.id = translations.question_id join speclalizations on questions.speclalization_id = speclalizations.id join users on questions.owner = users.id  where  speclalizations.name = :name and translations.dialect_id IS NULL OR translations.dialect_id =:dialect_id GROUP BY questions.id ,users.id,speclalizations.id,translations.dialect_id ORDER BY questions.date DESC ', { replacements: { name, dialect_id } },
   );
 
   return questionsData;
