a6ad8405acbd7100c4c0e234afb7bd6dfc83a5da
ali <ali.7@outlook.com>
2018-12-08 18:35:28 +0200

    change-text-of-about-us-section relate #302

--- client/src/components/HomePage/AboutUs/index.jsx
+++ client/src/components/HomePage/AboutUs/index.jsx
@@ -9,10 +9,7 @@ export default function AboutUs() {
         <h1 className="about__tittel">About Us</h1>
         <hr className="about_line" />
         <p className="about__text">
-           It is dangerous for someone to give or receive medical
-           information in their second language. Twig aims at resolving
-           this problem by providing reliable and accessible medical translation
-           from native people.
+          Twig is a database of common medical questions translated by volunteer medical professionals and other translators. Twig volunteers also verify each translation’s quality, and may add new questions to the system. Once verified multiple times, translations are made available to medical professionals to use. In this way Twig is a typical crowdsourced wiki whose vision is directed by its members.
         </p>
       </div>
       <img className="about__image" src={about} alt="about" />
