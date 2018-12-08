973fe63b759154c84537e36a86a49499ce6a988b
ali <ali.7@outlook.com>
2018-12-08 18:24:38 +0200

    change-homepage-img-and-add-some-text relate #302

--- client/src/components/HomePage/index.jsx
+++ client/src/components/HomePage/index.jsx
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
 import AboutUs from './AboutUs';
 import Statistic from './Statistic';
+import homeImg from './volunteer.jpg';
 import './style.css';
 
 const HomePage = () => (
   <Fragment>
-    <div className="home__image">
-      <h1 className="home__text">Medical Translation</h1>
+    <div className="home__image-container">
+      <img className="home__image" src={homeImg} alt="home" />
+      <p className="home__paragraph">&quot;We seek to provide written and audio translations for as many of the world’s 7,000+ languages as possible, in addition to video translations for as many of the world’s signed languages as possible for deaf communities&quot;</p>
+      <h1 className="home__text">Twig: The Medical Database</h1>
     </div>
     <div className="home__main">
       <div className="home__main--background">
