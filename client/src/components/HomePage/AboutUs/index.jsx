5c3699752b1b45f0c3c87a67ff56a28392c1a7ca
ali <ali.7@outlook.com>
2018-12-08 18:39:12 +0200

    covert-AboutUs-statless-function-from-ES5-to-ES6 relate #302

--- client/src/components/HomePage/AboutUs/index.jsx
+++ client/src/components/HomePage/AboutUs/index.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './style.css';
 import about from './aboutUs.jpeg';
 
-export default function AboutUs() {
-  return (
-    <div className="about">
-      <div>
-        <h1 className="about__tittel">About Us</h1>
-        <hr className="about_line" />
-        <p className="about__text">
-          Twig is a database of common medical questions translated by volunteer medical professionals and other translators. Twig volunteers also verify each translation’s quality, and may add new questions to the system. Once verified multiple times, translations are made available to medical professionals to use. In this way Twig is a typical crowdsourced wiki whose vision is directed by its members.
-        </p>
-      </div>
-      <img className="about__image" src={about} alt="about" />
+const AboutUs = () => (
+  <div className="about">
+    <div>
+      <h1 className="about__tittel">About Us</h1>
+      <hr className="about_line" />
+      <p className="about__text">
+        Twig is a database of common medical questions translated by volunteer medical professionals and other translators. Twig volunteers also verify each translation’s quality, and may add new questions to the system. Once verified multiple times, translations are made available to medical professionals to use. In this way Twig is a typical crowdsourced wiki whose vision is directed by its members.
+      </p>
     </div>
-  );
-}
+    <img className="about__image" src={about} alt="about" />
+  </div>
+);
+
+export default AboutUs;
