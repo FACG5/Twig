faa21eea36dead7e695deb98879e852e92161566
ali <ali.7@outlook.com>
2018-12-08 22:13:27 +0200

    add-links-to-cards relate #299

--- client/src/components/HomePage/Cards/index.jsx
+++ client/src/components/HomePage/Cards/index.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 
@@ -6,18 +7,24 @@ const Cards = () => (
   <Fragment>
     <h1 className="cards__title">What Can You Do On Twig</h1>
     <div className="cards__boxes">
-      <div className="cards__box">
-        <h3 className="cards__box-title">Translation Categories</h3>
-        <p>In the translation categories section, you can view translation categories and choose the category you interested to translate</p>
-      </div>
-      <div className="cards__box">
-        <h3 className="cards__box-title">Questions</h3>
-        <p>In the questions section, you can view questios of the category that you choose, and you can add and edit your own questions</p>
-      </div>
-      <div className="cards__box">
-        <h3 className="cards__box-title">Translations & Verifications</h3>
-        <p>In the translations section, you can view the translations of specific question depends on your language and dialect that you choose in the registration and you can add your own text/audio/video translation to it, also you can verify other volunteers translation of the same question</p>
-      </div>
+      <Link to="/main/">
+        <div className="cards__box">
+          <h3 className="cards__box-title">Translation Categories</h3>
+          <p>In the translation categories section, you can view translation categories and choose the category you interested to translate</p>
+        </div>
+      </Link>
+      <Link to="/main/">
+        <div className="cards__box">
+          <h3 className="cards__box-title">Questions</h3>
+          <p>In the questions section, you can view questios of the category that you choose, and you can add and edit your own questions</p>
+        </div>
+      </Link>
+      <Link to="/main/">
+        <div className="cards__box">
+          <h3 className="cards__box-title">Translations & Verifications</h3>
+          <p>In the translations section, you can view the translations of specific question depends on your language and dialect that you choose in the registration and you can add your own text/audio/video translation to it, also you can verify other volunteers translation of the same question</p>
+        </div>
+      </Link>
     </div>
   </Fragment>
 );
