ed0ba240d89ca700b4dd2977c67e380cbf7be2ba
ali <ali.7@outlook.com>
2018-12-08 21:22:18 +0200

    add-cards-function-to-Cards-component relate #299

---
+++ client/src/components/HomePage/Cards/index.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import './style.css';
+
+
+const Cards = () => (
+  <Fragment>
+    <h1 className="cards__tittel">What Can You Do On Twig</h1>
+    <div className="cards__boxes">
+      <div className="cards__box">
+        <h3>Translation Categories</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
+      </div>
+      <div className="cards__box">
+        <h3>Questions</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
+      </div>
+      <div className="cards__box">
+        <h3>Translations & Verifications</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
+      </div>
+    </div>
+  </Fragment>
+);
+
+export default Cards;
