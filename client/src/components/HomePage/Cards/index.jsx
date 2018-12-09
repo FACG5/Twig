9792d2ff1848934c4338566517932a74e12388e8
ali <ali.7@outlook.com>
2018-12-09 10:39:32 +0200

    add-icons-to-cards relate #299

--- client/src/components/HomePage/Cards/index.jsx
+++ client/src/components/HomePage/Cards/index.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './style.css';
 
 
@@ -9,18 +10,27 @@ const Cards = () => (
     <div className="cards__boxes">
       <Link to="/main/">
         <div className="cards__box">
+          <div className="cards__icon">
+            <FontAwesomeIcon icon="notes-medical" size="4x" />
+          </div>
           <h3 className="cards__box-title">Translation Categories</h3>
           <p>In the translation categories section, you can view translation categories and choose the category you interested to translate</p>
         </div>
       </Link>
       <Link to="/main/">
         <div className="cards__box">
+          <div className="cards__icon">
+            <FontAwesomeIcon icon="question-circle" size="4x" />
+          </div>
           <h3 className="cards__box-title">Questions</h3>
           <p>In the questions section, you can view questios of the category that you choose, and you can add and edit your own questions</p>
         </div>
       </Link>
       <Link to="/main/">
         <div className="cards__box">
+          <div className="cards__icon">
+            <FontAwesomeIcon icon="language" size="4x" />
+          </div>
           <h3 className="cards__box-title">Translations & Verifications</h3>
           <p>In the translations section, you can view the translations of specific question depends on your language and dialect that you choose in the registration and you can add your own text/audio/video translation to it, also you can verify other volunteers translation of the same question</p>
         </div>
