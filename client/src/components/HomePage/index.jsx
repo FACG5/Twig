63b1902e9bd75b2da2b7fa75989840a7c7f9c6c6
ali <ali.7@outlook.com>
2018-12-09 10:42:58 +0200

    remove-text-in-homepage-img relate #302

--- client/src/components/HomePage/index.jsx
+++ client/src/components/HomePage/index.jsx
@@ -3,15 +3,13 @@ import AboutUs from './AboutUs';
 import Statistic from './Statistic';
 import Goals from './Goals';
 import Cards from './Cards';
-import homeImg from './volunteer.jpg';
+import homeImg from './volunteer.png';
 import './style.css';
 
 const HomePage = () => (
   <Fragment>
     <div className="home__image-container">
       <img className="home__image" src={homeImg} alt="home" />
-      <p className="home__paragraph">&quot;We seek to provide written and audio translations for as many of the world’s 7,000+ languages as possible, in addition to video translations for as many of the world’s signed languages as possible for deaf communities&quot;</p>
-      <h1 className="home__text">Twig: The Medical Database</h1>
     </div>
     <div className="home__main">
       <div className="home__main--background">
