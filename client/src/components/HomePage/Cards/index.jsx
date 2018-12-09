61ee8ea506cc38811e32a42f6db7d8f1dbb88e7d
ali <ali.7@outlook.com>
2018-12-09 10:53:12 +0200

    add-line-in-cards-after-title relate #299

--- client/src/components/HomePage/Cards/index.jsx
+++ client/src/components/HomePage/Cards/index.jsx
@@ -14,6 +14,7 @@ const Cards = () => (
             <FontAwesomeIcon icon="notes-medical" size="4x" />
           </div>
           <h3 className="cards__box-title">Translation Categories</h3>
+          <hr className="cards__line" />
           <p>In the translation categories section, you can view translation categories and choose the category you interested to translate</p>
         </div>
       </Link>
@@ -23,6 +24,7 @@ const Cards = () => (
             <FontAwesomeIcon icon="question-circle" size="4x" />
           </div>
           <h3 className="cards__box-title">Questions</h3>
+          <hr className="cards__line" />
           <p>In the questions section, you can view questios of the category that you choose, and you can add and edit your own questions</p>
         </div>
       </Link>
@@ -32,6 +34,7 @@ const Cards = () => (
             <FontAwesomeIcon icon="language" size="4x" />
           </div>
           <h3 className="cards__box-title">Translations & Verifications</h3>
+          <hr className="cards__line" />
           <p>In the translations section, you can view the translations of specific question depends on your language and dialect that you choose in the registration and you can add your own text/audio/video translation to it, also you can verify other volunteers translation of the same question</p>
         </div>
       </Link>
