18b7b3e8363d1049a0f8caefb3a148d4ae732165
ali <ali.7@outlook.com>
2018-12-10 14:51:22 +0200

    refactor-statistics-component relate #339

--- client/src/components/HomePage/Statistic/index.jsx
+++ client/src/components/HomePage/Statistic/index.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal';
+import axios from 'axios';
 import './style.css';
 import satistic from './satistic.jpeg';
 
 class Statistic extends Component {
-  state = {};
+  state = {
+    values: [],
+  };
+
+  componentWillMount() {
+    axios
+      .get('/api/v1/statistic')
+      .then((data) => {
+        const results = data.data;
+        // console.log(results);
+        this.setState({
+          values: results,
+        });
+      })
+      .catch((error) => {
+        const { data: message, status } = error.response;
+        if (status === 500) {
+          this.setState({ message });
+        }
+      });
+  }
 
   render() {
+    const { values } = this.state;
+    const usersCount = values[0];
+    const users = usersCount && usersCount.usersCount;
+    const questionsCount = values[1];
+    const questions = questionsCount && questionsCount.questionsCount;
+    const translationsCount = values[2];
+    const translations = translationsCount && translationsCount.translationsCount;
     return (
       <Fade left>
         <div>
@@ -17,15 +45,15 @@ class Statistic extends Component {
           <img className="satistic__image" src={satistic} alt="satistic" />
           <div>
             <div className="satistic__Volunteers">
-              <h1 className="number">30</h1>
+              <h1 className="number">{users}</h1>
               <h2 className="satistic__text">Volunteers</h2>
             </div>
             <div className="satistic__Questions">
-              <h1 className="number">400</h1>
+              <h1 className="number">{questions}</h1>
               <h2 className="satistic__text">Questions</h2>
             </div>
             <div className="satistic__Translations">
-              <h1 className="number">4500</h1>
+              <h1 className="number">{translations}</h1>
               <h2 className="satistic__text">Translations</h2>
             </div>
           </div>
