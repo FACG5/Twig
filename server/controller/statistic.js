54ace57eb05a6a5f79f748346d1af95d7b2b86c8
ali <ali.7@outlook.com>
2018-12-10 12:39:42 +0200

    add-get-statistic-function-to-statistic relate #338

---
+++ server/controller/statistic.js
@@ -0,0 +1,33 @@
+const sequelize = require('../database/config');
+const { questions, users, translations } = require('../database/models');
+
+exports.get = async (request, response) => {
+  try {
+    const getUsersCount = await users.findAll({
+      logging:console.log,
+      attributes: [[sequelize.fn('COUNT', sequelize.col('id')), 'usersCount']],
+    });
+    const getQuestionsCount = await questions.findAll({
+      logging:console.log,
+      attributes: [[sequelize.fn('COUNT', sequelize.col('id')), 'questionsCount']],
+    });
+    const getTranslationsCount = await translations.findAll({
+      logging:console.log,
+      attributes: [[sequelize.fn('COUNT', sequelize.col('id')), 'translationsCount']],
+    });
+
+    const usersCount = getUsersCount[0].dataValues;
+    const questionsCount = getQuestionsCount[0].dataValues;
+    const translationsCount = getTranslationsCount[0].dataValues;
+
+    Promise.all([usersCount, questionsCount, translationsCount]).then((res) => {
+      if (!res.length) {
+        response.status(404).send('Oops! No data ! ');
+      } else {
+        response.status(200).send(res);
+      }
+    });
+  } catch (error) {
+    response.status(500).send('Server Error');
+  }
+};
