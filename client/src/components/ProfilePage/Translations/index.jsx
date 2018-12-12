ca3d4a43bcfcef1c5a2c8c3a042ee0f665f936a0
ali <ali.7@outlook.com>
2018-12-12 11:41:22 +0200

    change-component-to-stateful relate #357

--- client/src/components/ProfilePage/Translations/index.jsx
+++ client/src/components/ProfilePage/Translations/index.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-export default function Translations() {
-  return (
-    <div className="translations__box">
-      <h3 className="translations__title">Translation</h3>
-      <hr />
-      <div className="translations__content">
-        <p>
-          <h4>Under development</h4>
-        </p>
+class Translations extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div className="translations__box">
+        <h3 className="translations__title">Translation</h3>
+        <hr />
+        <div className="translations__content">
+          <p>
+            <h4>Under development</h4>
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Translations;
